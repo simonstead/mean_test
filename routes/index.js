85487386ac7f695ec68897af9d4a62cadf71d9c4
Simon Stead <simonstead@me.com>
2016-07-19 19:18:04 +0100

    Add w3.css framework, and refactor angular and three.js apps to separate views. Added corresponding get requests to index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/angular', function(req, res, next) {
+  res.render('angular', { title: 'A demo Angular app' });
+});
+
+/* GET home page. */
+router.get('/threejs', function(req, res, next) {
+  res.render('threejs', { title: 'Three.js Demo' });
+});
+
 module.exports = router;
